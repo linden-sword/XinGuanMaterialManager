56698d58f009d23a8a7cfd432a32f9d37172758c
un <2119530470@qq.com>
2021-12-10 17:43:53 +0800

    FQQ_V_3.0.1

--- src/main/java/com/zs/xinguanmaterialmanager/controller/BizSupplierController.java
+++ src/main/java/com/zs/xinguanmaterialmanager/controller/BizSupplierController.java
@@ -1,13 +1,8 @@
 package com.zs.xinguanmaterialmanager.controller;
 
-import com.github.pagehelper.PageInfo;
 import com.zs.xinguanmaterialmanager.entity.BizSupplier;
 import com.zs.xinguanmaterialmanager.service.impl.BizSupplierServiceImpl;
 import com.zs.xinguanmaterialmanager.util.R;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,22 +17,14 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/XinGuan/supplier")
-@Api(tags = "物资来源管理接口",value = "BizSupplierController")
+
 public class BizSupplierController {
     //fan
     @Autowired
     private BizSupplierServiceImpl bizSupplierService;
-    /**
-     * 添加物资信息
-     * @param bizSupplier 物资对象
-     * @return
-     */
+
     @PostMapping("/add")
-    @ApiOperation(value = "物资添加",notes = "Material addition")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "Id", value = "物资编号"),
-            @ApiImplicitParam(name = "Name", value = "物资名称")
-    })
+
     public R insert(@RequestBody BizSupplier bizSupplier){
         int i = bizSupplierService.insert(bizSupplier);
         if (i==1){
@@ -72,8 +59,8 @@ public class BizSupplierController {
      * @return
      */
     @GetMapping("/edit")
-    public BizSupplier queryById(Long id){
-        return bizSupplierService.queryById(id);
+    public R queryById(Long id){
+        return R.ok().setData(bizSupplierService.queryById(id));
     }
 
 
@@ -86,7 +73,7 @@ public class BizSupplierController {
     public R update(@RequestBody BizSupplier bizSupplier){
         int i = bizSupplierService.update(bizSupplier);
         if (i==1){
-            return  R.ok().setData("删除成功!");
+            return  R.ok().setData("");
         }
         if(i != 1){
             return  R.fail().setData("删除失败!");
@@ -101,8 +88,8 @@ public class BizSupplierController {
      * @return
      */
     @GetMapping("/findAll")
-    public PageInfo<BizSupplier> findAll(int pageNum,int pageSize){
-        return bizSupplierService.findAll(pageNum,pageSize);
+    public R findAll(int pageNum,int pageSize){
+        return R.ok().setData(bizSupplierService.findAll(pageNum,pageSize));
     }
 
     /**
@@ -113,11 +100,11 @@ public class BizSupplierController {
      * @return
      */
     @GetMapping("/findSupplierList")
-    public PageInfo<BizSupplier> findSupplierList(int pageNum,int pageSize,BizSupplier bizSupplier){
+    public R findSupplierList(int pageNum,int pageSize,BizSupplier bizSupplier){
         Map map =new HashMap();
         map.put("pageNum",pageNum);
         map.put("pageSize",pageSize);
-        return bizSupplierService.findSupplierList(map,bizSupplier);
+        return R.ok().setData(bizSupplierService.findSupplierList(map,bizSupplier));
     }
 //fan
 }
