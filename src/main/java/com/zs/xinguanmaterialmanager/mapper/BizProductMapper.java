444e0b4d604b8509ae172e5c571555b7c4633ba1
un <2119530470@qq.com>
2021-12-10 14:45:46 +0800

    FQQ_V_3.0.0

--- src/main/java/com/zs/xinguanmaterialmanager/mapper/BizProductMapper.java
+++ src/main/java/com/zs/xinguanmaterialmanager/mapper/BizProductMapper.java
@@ -1,5 +1,6 @@
 package com.zs.xinguanmaterialmanager.mapper;
 
+import com.zs.xinguanmaterialmanager.entity.BizOutStock;
 import com.zs.xinguanmaterialmanager.entity.BizProduct;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
